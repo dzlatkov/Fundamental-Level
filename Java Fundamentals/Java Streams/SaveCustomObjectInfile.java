827816b9c19ee0b1bec45e9b2e41c380a1682389
Daniel Zlatkov <d.zlatkov47@gmail.com>
2016-03-30 14:30:53 +0300

    Delete SaveCustomObjectInfile.java

--- Java Fundamentals/Java Streams/SaveCustomObjectInfile.java
+++
@@ -1,7 +0,0 @@
-
-public class SaveCustomObjectInfile {
-    public static void main(String[] args) {
-        // too complicated for me
-        // If you have any suggestion for 6, 7 and 8 task I would like to see it :)
-    }
-}
